0607aad608b52e78dae5b814a64ad171a0112093
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2021-01-31 15:56:28 -0500

    Simplified tagline

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ module.exports = {
   url: "https://minimodular.systems/",
   title: "Minimodular",
   description:
-    "Ultralight, minimal, modular organization systems for the outdoors. Aspiring to help people to enjoy human-powered adventures neatly and safely.",
+    "Ultralight, minimal, modular gear for the outdoors. Aspiring to help everyone enjoy human-powered adventures neatly and safely.",
   author: "Benjamin Wiederkehr",
   authorURL: "https://benjaminwiederkehr.com",
   analytics: "", // put your google analytics code here to add tracking
